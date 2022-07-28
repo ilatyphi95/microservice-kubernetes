7dc764e931561590610d4e859095aeb9ad62010d
Opebiyi Abdul-Lateef <ilatyphi95@gmail.com>
2022-07-28 15:08:52 +0000

    Upload docker image

--- upload_docker.sh
+++ upload_docker.sh
@@ -5,11 +5,14 @@
 
 # Step 1:
 # Create dockerpath
-dockerpath=api
+username=ilatyphi95
+dockerpath=${username}/api
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login
+docker tag api ${dockerpath}
 
 # Step 3:
 # Push image to a docker repository
