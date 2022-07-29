4ceed84c58b59e84033d588950138c18f91397af
Opebiyi Abdul-Lateef <ilatyphi95@gmail.com>
2022-07-29 10:15:48 +0100

    Deploy kubernetes and save output logs

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,17 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=ilatyphi95/api
 
 # Step 2
 # Run the Docker Hub container with kubernetes
+kubectl create deploy housing-prediction --image=${dockerpath}
 
 
 # Step 3:
 # List kubernetes pods
+kubectl get deploy,rs,svc,pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward pod/housing-prediction-85cc8c68c7-r5fmv --address 0.0.0.0 8000:80
